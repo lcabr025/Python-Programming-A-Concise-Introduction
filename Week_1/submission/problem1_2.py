5e2b9e07ceab74984bc50ea54ea880dc0f72f5ed
lcabr025 <lcabr025@fiu.edu>
2020-08-31 20:35:08 -0400

    adding files used for submission of week 1 assignment

---
+++ Week_1/submission/problem1_2.py
@@ -0,0 +1,13 @@
+"""
+Problem 1_2:
+Write a function problem1_2(x,y) that prints the sum and product of the
+numbers x and y on separate lines, the sum printing first.
+"""
+#%%
+def problem1_2(x,y):
+
+    sum = x + y
+    print(sum)
+
+    product = x * y
+    print(product)
